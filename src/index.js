1783a4bc0832616725ce15c2530bb0aac49da24e
mahedihsharif <mahedi5061@gmail.com>
2026-05-02 20:20:35 +0600

    render-deployed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 // Entry point for Render deployment
 // This file redirects to the actual compiled server
-require('../dist/server');
+const path = require('path');
+require(path.resolve(__dirname, '../dist/server'));
